ca736eb284cbf36243b1f14da46dfd180cd4da79
forrestkirby <18228336+forrestkirby@users.noreply.github.com>
2020-10-16 16:04:36 +0200

    Update hd-image-comparison.js

--- hd-image-comparison/js/hd-image-comparison.js
+++ hd-image-comparison/js/hd-image-comparison.js
@@ -1,38 +1,60 @@
-.hd-image-comparison {
-	position: relative;
-	display: inline-flex;
+class hdImgComp {
+
+	constructor(el) {
+		this.element = el;
+		this.before = el.querySelector('.hd-image-comparison-before');
+		this.beforeimg = el.querySelector('.hd-image-comparison-before img');
+		this.afterimg = el.querySelector('.hd-image-comparison-after img');
+		this.slider = el.querySelector('.hd-image-comparison-slider');
+		this.range = document.createElement('input');
+		this.range.type = 'range';
+		this.range.min = '0';
+		this.range.max = '100';
+		this.range.value = (0 <= this.slider.dataset.start <= 100) ? this.slider.dataset.start : 50;
+		this.range.style.marginLeft = `calc(-1 * (${this.slider.dataset.margin}px + 20px) / 2)`;
+		this.range.style.width = `calc(100% + (${this.slider.dataset.width}px + 20px))`;
+		this.range.classList.add('hd-image-comparison-range');
+		el.insertBefore(this.range, this.slider);
+	}
+
+	init() {
+		// set initial sizes and positions
+		this.setSizePos();
+		// add event listeners
+		this.range.addEventListener('input', () => { this.slide() });
+		this.range.addEventListener('change', () => { this.slide() });
+		window.addEventListener('resize', () => { this.setSizePos() });
+		window.addEventListener('orientationchange', () => { this.setSizePos() });
+	}
+
+	setSizePos() {
+		// reset initial width of element container
+		this.element.style.width = 'auto';
+		// set initial size of before image container
+		this.before.style.height = `${this.afterimg.offsetHeight}px`;
+		this.before.style.width = `${this.range.value}%`;
+		// set initial width of element container
+		this.element.style.width = `${this.afterimg.offsetWidth}px`;
+		// set initial width of before image
+		this.beforeimg.style.width = `${this.afterimg.offsetWidth}px`;
+		// set initial position of slider
+		this.slider.style.left = `${this.range.value}%`;
+	}
+
+	slide() {
+		// update width of before image container
+		this.before.style.width = `${this.range.value}%`;
+		// update position of slider
+		this.slider.style.left = `${this.range.value}%`;
+	}
+
 }
 
-.hd-image-comparison .hd-image-comparison-before {
-	position: absolute;
-	top: 0px;
-	left: 0px;
-	overflow: hidden;
-}
-
-.hd-image-comparison-before img {
-	max-width: none;
-}
-
-.hd-image-comparison .hd-image-comparison-slider {
-	padding: 10px;
-	border-radius: 50%;
-	position: absolute;
-	top: 50%;
-	left: 50%;
-	transform: translate(-50%, -50%);
-}
-
-.hd-image-comparison-slider svg {
-	max-width: none;
-}
-
-.hd-image-comparison-range {
-	position: absolute;
-	top: 0;
-	left: 0;
-	z-index: 1;
-	height: 100%;
-	width: 100%;
-	opacity: 0;
-}
+UIkit.util.$$('.hd-image-comparison-after img').forEach(function(el) {
+	el.addEventListener('load', (e) => {
+		let x = new hdImgComp(e.target.closest('.hd-image-comparison'));
+		x.init();
+	}, {
+		once: true
+	});
+});
